c23764d87f66a2ac2ba813a37716939b20ecbdaa
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2026-04-15 10:54:16 +0200

    feat: update storeLocale method to include Request parameter for better integration

--- src/Drivers/Concerns/DoesNotPersistLocale.php
+++ src/Drivers/Concerns/DoesNotPersistLocale.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Josemontano1996\LaravelOctaneLocalization\Drivers\Concerns;
 
+use Symfony\Component\HttpFoundation\Request;
+
 trait DoesNotPersistLocale
 {
-    public function storeLocale(string $locale): void
+    public function storeLocale(string $locale, Request $request): void
     {
         // Intentionally empty for No-op drivers
     }
