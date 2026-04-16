8c158fb75a185376cb5c293049828033e59d79fd
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2026-04-16 20:27:15 +0200

    testing: added DoesNotPersistLocaleTest

---
+++ tests/Unit/Drivers/Concerns/DoesNotPersistLocaleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Josemontano1996\LaravelOctaneLocalization\Drivers\Concerns\DoesNotPersistLocale;
+use Symfony\Component\HttpFoundation\Request;
+
+it('does absolutely nothing to the request or the environment', function () {
+    $tester = new class
+    {
+        use DoesNotPersistLocale;
+    };
+    $request = Request::create('/test');
+
+    // 1. Snapshot the state
+    $requestBefore = serialize(clone $request);
+    $localeBefore = app()->getLocale();
+
+    // 2. Act
+    $tester->storeLocale('fr', $request);
+
+    // 3. Assert total lack of change
+    expect(serialize($request))->toBe($requestBefore)
+        ->and(app()->getLocale())->toBe($localeBefore);
+});
