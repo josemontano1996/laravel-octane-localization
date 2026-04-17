47adac4671411f74dafbb63d02a89e6c05e09237
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2026-04-17 11:46:00 +0200

    feat: add IDE helper files for Blade directives and route macros

---
+++ src/Support/_ide_helper_blade_directives.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Illuminate\Support\Facades {
+    /**
+     * @method static void directive(string $name, callable $handler)
+     * @method static void if(string $name, callable $callback)
+     * @method static string currentLocale()
+     * @method static bool isLocale(string $locale)
+     * @method static void endisLocale()
+     * @method static void supportedLocales(string $code, string $data)
+     * @method static void endsupportedLocales()
+     * @method static string localizedUrl(string $locale)
+     * @method static string alternateLinks()
+     */
+    class Blade {}
+}
---
+++ src/Support/_ide_helper_macros.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Illuminate\Support\Facades {
+    /**
+     * @method static \Illuminate\Routing\RouteRegistrar localizedWithPrefix(callable|null $callback = null)
+     *
+     * @see \Josemontano1996\LaravelOctaneLocalization\Registrars\RegisterMacros
+     */
+    class Route {}
+}
+
+namespace Illuminate\Routing {
+    /**
+     * @method \Illuminate\Routing\RouteRegistrar localizedWithPrefix(callable|null $callback = null)
+     *
+     * @see \Josemontano1996\LaravelOctaneLocalization\Registrars\RegisterMacros
+     */
+    class Router {}
+}
